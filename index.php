36fa001787d402c561095f923d1162fc28bc7ba7
wcrvieira <wagner.vieira@etec.sp.gov.br>
2022-07-30 21:29:34 -0300

    Upload do projeto

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>O que eu mais gosto na minha cidade</title>
+    <link rel="stylesheet" href="css/estilo.css">
+</head>
+<body>    
+    <h2>Informações sobre malwares</h2>    
+    <div class="container">        
+        <div class="card">
+            <h2>Vírus</h2>
+            <img src="images/virus.png" alt="Vírus de computador">
+            <p> Um vírus de computador é um software malicioso que é desenvolvido 
+                por programadores geralmente inescrupulosos. Tal como um vírus biológico, 
+                o programa infecta o sistema, faz cópias de si e tenta se espalhar para outros
+                computadores e dispositivos de informática..</p>
+        </div>
+
+        <div class="card">
+            <h2>Worms</h2>
+            <img src="images/worms.png" alt="Worms">
+            <p> Um Worm é um programa semelhante aos vírus, com a diferença de este ser auto-replicante,
+                ou seja, ele cria cópias funcionais de si mesmo e infecta outros computadores. Tal infecção
+                pode ocorrer através de conexões de rede locais, Internet ou anexos de emails. 
+                Alguns worms infectaram centenas de computadores por meio das redes sociais MySpace e Facebook..</p>
+        </div>
+
+        <div class="card">
+            <h2>Trojan</h2>
+            <img src="images/trojan.png" alt="Cavalo de Tróia">
+            <p> Na informática, um cavalo de tróia (trojan horse) é um programa, normalmente recebido como um "presente"
+                (por exemplo, cartão virtual, álbum de fotos, protetor de tela, jogo, etc), que além de executar funções 
+                para as quais foi aparentemente projetado, também executa outras funções normalmente maliciosas e sem o 
+                conhecimento do usuário.
+            </p>
+        </div>    
+
+        <div class="card">
+            <h2>Spyware</h2>
+            <img src="images/spyware.png" alt="Espiões">
+            <p> é o termo utilizado para se referir a uma grande categoria de software que tem o objetivo de monitorar
+                atividades de um sistema e enviar as informações coletadas para terceiros.</p>
+        </div>
+
+        <div class="card">
+            <h2>Keylogger</h2>
+            <img src="images/keylogger.png" alt="Keylogger">
+            <p> Keylogger é um programa capaz de capturar e armazenar as teclas digitadas pelo usuário no teclado de um computador.
+                Um keylogger pode capturar e armazenar as teclas digitadas pelo usuário. </p>
+        </div>
+
+        <div class="card">
+            <h2>Ransomware</h2>
+            <img src="images/ransomware.png" alt="Ransomware">
+            <p> Ransomware é um tipo de código malicioso que torna inacessíveis os dados armazenados em um equipamento, geralmente usando
+                criptografia, e que exige pagamento de resgate (ransom) para restabelecer o acesso ao usuário.
+                O pagamento do resgate geralmente é feito via bitcoins.
+            </p>
+        </div>    
+
+    </div>
+    </body>
+    </html>
